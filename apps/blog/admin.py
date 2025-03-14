2cf78552ab3502deec7871d75311a24e59c1ec75
endang-ismaya <endang.ismaya@gmail.com>
2025-03-14 11:21:18 +0700

    add models and admin

--- apps/blog/admin.py
+++ apps/blog/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from apps.blog.models import Post
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ["title", "slug", "author", "publish_at", "status"]
+    list_filter = ["status", "created_at", "publish_at", "author"]
+    search_fields = ["title", "body"]
+    prepopulated_fields = {"slug": ("title",)}
+    raw_id_fields = ["author"]
+    date_hierarchy = "publish_at"
+    ordering = ["status", "publish_at"]
+    show_facets = admin.ShowFacets.ALWAYS
--- apps/blog/models.py
+++ apps/blog/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.utils import timezone
+from django.conf import settings
 
-# Create your models here.
+
+class Post(models.Model):
+    class Status(models.TextChoices):
+        DRAFT = "DF", "Draft"
+        PUBLISHED = "PB", "Published"
+
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250)
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="blog_posts",
+    )
+    body = models.TextField()
+    publish_at = models.DateTimeField(default=timezone.now)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=2, choices=Status, default=Status.DRAFT)
+
+    class Meta:
+        ordering = ("-publish_at",)
+        indexes = [
+            models.Index(fields=["-publish_at"]),
+        ]
+
+    def __str__(self):
+        return self.title
