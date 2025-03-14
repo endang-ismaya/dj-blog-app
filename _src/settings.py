8c90258cd905065b72d2dcff88f14470bad3e7c3
endang-ismaya <endang.ismaya@gmail.com>
2025-03-14 07:23:44 +0700

    add blog app

--- _src/settings.py
+++ _src/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # internal apps 
+    "apps.blog", 
     # 3rd party apps 
 ]
 
---
+++ apps/blog/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+
+from apps.blog import views
+
+
+
+urlpatterns = [
+    path("", views.index, name="blog__index"),
+]
